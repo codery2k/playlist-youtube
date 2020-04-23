bc6fa960e911c9cf70d692303d56270c0db9dee6
codery2k <95smiglani@gmail.com>
2020-04-24 01:42:33 +0530

    Add function to load tracking file

--- main.py
+++ main.py
@@ -5,22 +5,31 @@
 # https://developers.google.com/explorer-help/guides/code_samples#python
 
 import os
-
 import googleapiclient.discovery
 import googleapiclient.errors
-import googleapiclient.model
+# import csv
 
 api_service_name = "youtube"
 api_version = "v3"
 developerKey=os.getenv("YOUTUBE_DATA_API_KEY")
 yt_video_base_url="https://www.youtube.com/watch?v="
 max_results=3
+filename="track.csv"
+music_directory="/home/coder/Music/Assorted"
 
 class Yt_info:
     def __init__(self, title, uploader, link):
         self.title=title
         self.uploader=uploader
         self.link=link
+
+def load_track_file():
+    if(not(os.path.isfile(filename))):
+        files=os.listdir(music_directory)
+        with open(filename, 'w') as track_file:
+            # writer = csv.writer(file)
+            for file in files:
+                track_file.write(file+",0"+"\n")
 
 def print_yt_infos(query):
     yt_infos=get_yt_info(query)
@@ -32,7 +41,6 @@ def print_yt_info(yt_info):
     print(yt_info.uploader)
     print(yt_info.title)
     print()
-
 
 def get_yt_info(query):
     yt_response=hit_yt(query)
@@ -60,7 +68,9 @@ def hit_yt(query):
     return response    
 
 def main():
-    print_yt_infos("Wish you were here pink floyd topic")
+    # print_yt_infos("Wish you were here pink floyd topic")
+    load_track_file()
+    
 
 if __name__ == "__main__":
     main()
